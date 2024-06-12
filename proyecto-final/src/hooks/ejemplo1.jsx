1de12b56433c494e00dac32b2ef6c746680a4c98
Antonio Francisco Cutillas Garcia <antonicut@gmail.com>
2024-06-12 17:11:52 +0200

    Video 4

---
+++ proyecto-final/src/hooks/ejemplo1.jsx
@@ -0,0 +1,75 @@
+//  ***********************************************
+//  **********  /src/hooks/ejemplo1.jsx  **********
+//  ***********************************************
+
+/*
+    **********  Ejemplo de uso del Hook 'useState'  **********
+
+    Crear componente de tipo función y acceder a su estado 
+    privado a través de un hook y, ademas, poder modificarlo.
+
+*/
+
+import React, { useState } from 'react';
+
+
+const Ejemplo1 = () => {
+    
+    //  Valor inicial para un contador.
+    const valorInicial = 0
+
+    //  Valor inicial para una persona.
+    const personaInicial = {
+        nombre: 'Antonio',
+        email:  'antonicut@gmail.com'
+        
+    }
+
+    //  Queremos que el VALORINICIAL y PERSONAINICIAL sean
+    //  parte del estado del componente para asi poder gestionar su
+    //  cambio y que este se vea reflejado en la vista del componente.
+    
+    //  -----  Estructura  -----
+    //  const [nombreVariable, funcionParaCambiar] = useState(valorInicial).
+    
+    const [contador, setcontador] = useState(valorInicial)
+    const [persona, setPersona]   = useState(personaInicial)
+
+    
+    // Funcion para actualizar el estado privado que contiene el contador.
+    function incrementarContador() {
+
+        //  funcionParaCambiar(nuevoValor).
+        setcontador(contador + 1)
+    }
+
+    // Funcion para actualizar el estado de persona en el componente. 
+    function actualizarPersona() {
+        setPersona({
+            nombre: 'Pepe',
+            email:  'pepeluis@gmail.com'
+        })
+    }
+
+    
+    return (
+        <div>
+            
+            <h1> *** Ejemplo 1 de useState() *** </h1>
+            <h2> Contador: {contador} </h2>
+            <h2> Datos de la Persona: </h2>
+            <h3> Nombre: {persona.nombre} </h3>
+            <h3> Email: {persona.email} </h3>
+
+            { /*  Bloque de botones para actualizar el estado del componente  */}
+            <button onClick={incrementarContador}> Incrementar Contador </button>
+            <button onClick={actualizarPersona}> Actualizar Persona </button>
+            <br></br> <br></br>
+
+        </div>
+    );
+}
+
+export default Ejemplo1;
+
+
---
+++ proyecto-final/src/hooks/ejemplo2Caso3.jsx
@@ -0,0 +1,71 @@
+//  ****************************************************
+//  **********  /src/hooks/ejemplo2Caso3.jsx  **********
+//  ****************************************************
+
+/*
+    **********  Ejemplo de uso de los siguientes hooks  **********
+
+    -  useState().
+    -  useRef(),     Identificar o referenciar elementos o valores dentro de la vista.
+    -  useEffect(),  Nos ayuda a controlar los cambios en la vista.
+
+*/
+
+
+import React, { useState, useRef, useEffect } from 'react';
+
+
+const Ejemplo2Caso3 = () => {
+
+    //  Crear dos contadores distintos, cada uno en un estado diferente.
+    const [contador1, setContador1] = useState(0)
+    const [contador2, setContador2] = useState(0)
+
+    //  Vamos a crear una referencia con useRef() para asociar una variable
+    //  con un elemento del DOM del componente (vista HTML).
+    const miRef = useRef();
+
+    function incrementar1() {
+        setContador1(contador1 + 1)
+    }
+
+    function incrementar2() {
+        setContador2(contador2 + 1)
+    }
+
+
+    //  Trabajando con UseEffect().
+
+    //  -----  Caso 3: Ejecutar SOLO cuando cambie contador 1 o Contador 2 -----
+
+    //    Cada vez que haya yn cambio en contador 1, se ejecutara lo que diga useEffect()
+    //    Cada vez que haya yn cambio en contador 2, se ejecutara lo que diga useEffect()
+
+    useEffect(() => {
+        console.log("*****  ejemplo2Caso3  *****")
+        console.log('CAMBIO EN EL ESTADO DEL CONTADOR 1 o CONTADOR 2')
+        console.log('Mostrando referencia a elemento del DOM')
+        console.log({ miRef })
+    }, [contador1, contador2]);
+
+
+    return (
+        <div>
+
+            <h1> *** Ejemplo 2 - Caso 3 - de useState(), useRef() y useEffect() *** </h1>
+            <h2> Contador 1: {contador1} </h2>
+            <h2> Contador 2: {contador2} </h2>
+
+            { /*  Elemento Referenciado */}
+            <h4 ref={miRef}> Ejemplo de elemento referenciado </h4>
+
+            { /*  Bloque de botones para actualizar el estado del componente  */}
+            <button onClick={incrementar1}> Incrementar Contador 1 </button>
+            <button onClick={incrementar2}> Incrementar Contador 2 </button>
+            <br></br> <br></br>
+
+        </div>
+    )
+}
+
+export default Ejemplo2Caso3
